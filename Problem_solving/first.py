9d31f7eed0ef26ba8505105b99d4b91c12e15e29
Afreen <afreen2504267@st.jmi.ac.in>
2026-01-04 23:21:36 +0530

    Problem Solving Examples

---
+++ Problem_solving/first.py
@@ -0,0 +1,6 @@
+# Calculating the Average of three numbers
+a = int(input("What is the value of a: "))
+b = int (input("What is the value of b: "))
+c =int(input("What is the value of c: "))
+avg = (a+b+c)/3
+print(f"Average of a, b and c is: {avg}")
