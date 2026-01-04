69685aaac09273040a534de0b2882e9e60671602
Afreen <afreen2504267@st.jmi.ac.in>
2026-01-04 23:48:20 +0530

    Problem Solving Example

--- first.py
+++ first.py
@@ -1,18 +1,21 @@
-name = input("What is your name? ")
-age = input("What is your age ")
-# For multiple lines use multiple print()
-"""print("Hello", name)
-print("You are", age, "years old. ")
+# Variable in Python
+# variable is memory location in computer stores the values (container)
+"""Rules of variables :
+1. Only Digits, Alphabets and underscores are permitted. 
+2. Variable name should start with underscore and alphabets only.
+3. No white space between variable name.
+4. No special character is permitted to use.
 """
-#For single line output use single print()
-"""print("Hello", name , " you are", age, "years old")"""
-#Use end='\n'
-#after end only one string is allowed
-#wrong: print("Hello", name , end= '\n' + " you are", age, "years old")
-#right way:
-print("Hello", name , end= '\n')
-print("You are", age, "years old")
 
+# Below Example : "name" is a variable that stores a String (str)
+name = input("What's your Name? ")  # Takes input from the User
+print(name)                         # Prints Name of the user
 
+# Some special functions for improving the user input :
+#  strip() = It removes and extra white space present between user input characters.
+#  title() = It Capatilised the string first letter of each word.
+
+Name = input("What is your name? ").strip().title()
+print("Hello,", Name)  
 
 
